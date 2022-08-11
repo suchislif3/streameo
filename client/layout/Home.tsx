9cc171ebd078deed5f17e1fe8a9994260e05ca83
Szabolcs Devecseri <sz.devecseri@gmail.com>
2022-08-11 18:09:33 +0200

    layout set up

---
+++ client/layout/Home.tsx
@@ -0,0 +1,29 @@
+import { AppShell, Box, Header, Navbar } from "@mantine/core";
+import Image from "next/image";
+import React from "react";
+
+function HomePageLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <AppShell
+      padding="md"
+      navbar={
+        <Navbar width={{ base: 300 }} height={500} p="xs">
+          Side items
+        </Navbar>
+      }
+      header={
+        <Header height={60} p="xs">
+          <Box>
+            <Box>
+              <Image src="/logo.png" alt="logo" width="100px" height="40px" />
+            </Box>
+          </Box>
+        </Header>
+      }
+    >
+      {children}
+    </AppShell>
+  );
+}
+
+export default HomePageLayout;
